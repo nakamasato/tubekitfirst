009627792dfdd093b40877b813a2fe009478180c
nakamasato <utjxs.ntn@hotmail.co.jp>
2011-10-20 13:38:46 +0900

    changed input table

--- backbone/stop.php
+++ backbone/stop.php
@@ -6,14 +6,21 @@ $contents = @file_get_contents('stopword.txt');
 $contents = explode(' ',$contents);
 
 $size = count($contents);
-echo $size;
 for($i = 0; $i < $size; $i++){
 $contents[$i] = trim($contents[$i]);
-$hash[$content[$i]]='1';
-echo($contents[$i]."\n");
-}
-var_dump($hash);
+$key = $contents[$i];
+$hash[$key]='1';
 
+}
+
+
+$input1 =  fgets(STDIN,4096);
+$input1 = trim($input1);
+if (empty($hash[$input1])){
+echo" is not a stopword\n";
+}else{
+echo"is a stopword\n";
+}
 
 
 ?>
