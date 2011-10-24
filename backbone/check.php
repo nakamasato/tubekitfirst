52eabc09b571d630a28b062a90d64cd580d75e5d
nakamasato <utjxs.ntn@hotmail.co.jp>
2011-10-24 15:05:15 +0900

    improved

--- backbone/check.php
+++ backbone/check.php
@@ -7,7 +7,6 @@
   require_once("stop.php");
         $sql = "SHOW TABLES FROM backbone";
         $result = mysql_query($sql);
-        echo "which table do you want to use as keyword? Select the number.\n";
         $number = 0;
         $table = array();
         while ($row = mysql_fetch_row($result)) {
@@ -15,6 +14,7 @@
             $table[$number] = $row[0];
             $number++;
         }
+        echo "Select a new table.\n";
         $input = (int) fgets(STDIN,4096);
         $query = "SELECT distinct keyword FROM $table[$input]";
         $vresult = mysql_query($query) or die(" ". mysql_error());
@@ -23,6 +23,8 @@
         {   
 	    $ke[$i]=$line['keyword'];
 	    $key = $line['keyword'];
+	    $ke[$i]=strtolower($key);
+	    $key=strtolower($key);
             $v[$key] = "1";
 	    $i++;
 	}
@@ -30,7 +32,6 @@
 
         $sql = "SHOW TABLES FROM backbone";
         $result = mysql_query($sql);
-        echo "which table do you want to use as keyword? Select the number.\n";
         $number = 0;
         $table = array();
         while ($row = mysql_fetch_row($result)) {
@@ -38,6 +39,7 @@
             $table[$number] = $row[0];
             $number++;
         }
+        echo "Select a old table.\n";
         $input = (int) fgets(STDIN,4096);
         $query = "SELECT distinct keyword FROM $table[$input]";
         $vresult = mysql_query($query) or die(" ". mysql_error());
@@ -45,10 +47,11 @@
 	while ($line = mysql_fetch_array($vresult, MYSQL_ASSOC)) //Returns an array of strings that corresponds to the fetched row
         {   
             $v2[$j] = $line['keyword'];
+	    $v2[$j] = strtolower($v2[$j]);
 	    $j++;
 	}
 	$size2 = $j;
-
+	$exist=0;
 	$plural=0;
 for($j=0;$j<$size2;$j++){
         $k=$v2[$j];
@@ -56,25 +59,30 @@ for($j=0;$j<$size2;$j++){
 	   echo($k." doesnt exist in new table.\n");
 		  $plural++;
 	}else{
-
+	   echo($k."\n");
+		$exist++;
 	}
 }
 echo"\n";
-for($j=0;$j<$size;$j++){
+$stopold=0;
+for($j=0;$j<$size2;$j++){
         $k=$v2[$j];
 
 	if (!empty($hash[$k])){
 	   echo($k." is a stopword in the old table.\n");
+	   $stopold++;
 	   continue;
 	   echo("after continue\n");
 	}
-	echo($j."\n");
+//	echo($j."\n");
 }
 echo"\n";
+$stopnew=0;
 for($i=0;$i<$size;$i++){
         $k=$ke[$i];
 	if (!empty($hash[$k])){
 	   echo($k." is a stopword in the new table.\n");
+	   $stopnew++;
 	}
 }
 echo"\n";
@@ -92,12 +100,12 @@ echo"\n";
 		}
 	}
 */
-	echo($size."\n");
-	echo($size2."\n");
-	echo($plural."\n");
-
-
-
+	echo($size."keywords in new tables\n");
+	echo($size2."keywords in old  tables\n");
+	echo($plural."keywords in old but not in new\n");
+	echo($stopnew."stopwords in new\n");
+	echo($stopold."stopwords in old\n");
+	echo($exist. "keywords in both\n");
 
 
 
--- backbone/stoprenshu.php
+++ backbone/stoprenshu.php
@@ -1,10 +1,40 @@
 <?php
 
 require_once("stop.php");
+require_once("config.php");
+$query = "select distinct keyword from 2011_";
+$result = mysql_query($query);
+$not = 0;
+$stop = 0;
+$space = 0;
+while($line = mysql_fetch_array($result)){
+	    $keyword = $line['keyword'];
+	    $keyword = strtolower($keyword);
+	    $keyword = trim($keyword);
+	    if(preg_match("/\s/",$keyword)){
+	    $space++;	    
+	    continue;
+	    }
+	    if(empty($hash[$keyword])){
+	    echo($keyword. " is not a stopword.\n");
+	    $not++;
+	    }else{
+	    echo($keyword. " is a stopword.\n");
+	    $stop++;
+	    }
+}
+echo($not."nonstopwords\n");
+echo($stop."stopwords\n");
+echo($space."spaces\n");
 
-var_dump($hash);
-for($i=0;$i<5;$i++){
+
+
+
+
+//check 3 word if they are stopword or not
+for($i=0;$i<4;$i++){
 $input1 =  fgets(STDIN,4096);
+$input1 = strtolower($input1);
 $input1 = trim($input1);
 if (empty($hash[$input1])){
 continue;
@@ -13,6 +43,7 @@ echo"is a stopword\n";
 }
 echo"ここは？\n";
 }
+
 /*
 $sum = 0;
 for($i=1;$i<10;$i++){
